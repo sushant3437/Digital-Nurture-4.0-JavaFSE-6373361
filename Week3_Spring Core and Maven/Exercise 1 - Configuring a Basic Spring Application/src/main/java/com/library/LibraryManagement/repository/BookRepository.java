41fd3f1fa5a37f804b1dea6f5f8eedd95253fb23
sushant <sushantpatil3437@gmail.com>
2025-07-03 12:57:11 +0530

    Added Spring Core and Maven Exercises

---
+++ Week3_Spring Core and Maven/Exercise 1 - Configuring a Basic Spring Application/src/main/java/com/library/LibraryManagement/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.LibraryManagement.repository;
+
+public class BookRepository {
+    public void saveBook(String title) {
+        System.out.println("Book '" + title + "' saved to repository.");
+    }
+}
---
+++ Week3_Spring Core and Maven/Exercise 4 - Creating and Configuring a Maven Project/src/main/java/com/library/LibraryManagement/App.java
@@ -0,0 +1,8 @@
+package com.library.LibraryManagement;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven and Spring setup complete!");
+    }
+}
